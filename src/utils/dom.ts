862d0b7c7459fba66bcb4256be728c5687ce15cb
YeonghunKO <yhko1988@gmail.com>
2022-09-06 19:32:35 +0900

    Add remove method inside prototype of Element

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,7 +1,28 @@
+declare global {
+  interface NodeList {
+    remove(): any;
+  }
+  interface HTMLCollection {
+    remove(): any;
+  }
+}
+
 type CreateElement = {
   type: string;
   classes?: string[];
   id?: string;
+};
+
+Element.prototype.remove = function () {
+  this.parentElement.removeChild(this);
+};
+
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+  for (var i = this.length - 1; i >= 0; i--) {
+    if (this[i] && this[i].parentElement) {
+      this[i].parentElement.removeChild(this[i]);
+    }
+  }
 };
 
 const createElement = ({ type, classes, id }: CreateElement): HTMLElement => {
